0b8eedd1e0f0c3dd5a4299eb165c2b7b1a8fa459
Sergii Morozov <sergmarkus@gmail.com>
2024-07-25 19:30:14 +0200

    all

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,25 @@
-function makeTransaction(quantity, pricePerDroid) {
-    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
-};
+function slugify(title) {
+    return title
+        .split(" ")
+        .join("-")
+        .toLowerCase();
+    
+}
+console.log(slugify("Arrays for begginers"));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+// function slugify(title) {
+
+//     const wordsArr = title.split(" ");
+//     const wordsStr = wordsArr.join("-");
+//     const wordLowerCase = wordsStr.toLowerCase();
+
+//     return wordLowerCase; 
+// }
+// console.log(slugify("Arrays for begginers"));
+// console.log(slugify("English for developer"));
+// console.log(slugify("Ten secrets of JavaScript"));
+// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
